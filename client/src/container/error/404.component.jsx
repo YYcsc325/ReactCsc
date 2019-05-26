a28cd9a3f88d68fe76ebdbda8ce41b64782d8951
YYCSC <yycsc@YYCSCdeMBP.lan>
2019-05-26 19:37:05 +0800

    modify

--- client/src/container/error/404.component.jsx
+++ client/src/container/error/404.component.jsx
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
+import { observer } from 'mobx-react';
+import State from './index.state';
+import { person , show } from '../../utils/component'
+import Header from '../../utils/components'
 
-export default class Index extends Component {
+@observer
+class Index extends Component {
+    constructor(props){    // extends加上constructor   一定要加上super
+        super();
+        this.state = {
+            
+        }
+    }
+    change = data => {
+        State.changeQueryData();
+    }
+    changed = data => {
+        console.log('触发了')
+    }
+    myClick = data => {
+        console.log('点击事件触发了')
+    }
+    handletoggle = () => {
+        console.log('handletoggle')
+    }
+    componentWillMount(){
+         
+    }
     render() {
         return (
             <div>
-                404
+                { State.queryData }
+                <button onClick = { this.change }>点击一下</button> 
+                <Header 
+                    handletoggle = { this.handletoggle }
+                    title = { 'baby' }//在有状态的组建中，接受父组件传来的数据要加this
+                    style = { { color:"pink" } }
+                />
+                {
+                    person({
+                        "name":'岑世超',
+                        'changed':this.changed,
+                        'myClick':this.myClick
+                    })
+                }
+                {
+                    show(
+                        "洪盛",
+                        {
+                            "style":{
+                                "color":"red",
+                                'fontSize':"24px"
+                            },
+                            "onClick":function(){
+                                console.log('我变漂亮了')
+                            }
+                        }
+                    )
+                }
             </div>
         )
     }
 }
+export default Index;
--- client/src/container/userManager/addUser/index.component.jsx
+++ client/src/container/userManager/addUser/index.component.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-
+import './index.css'
 class Index extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            arr:[]
+        }
     }
 
     componentWillMount() {
-
+        setTimeout(function(){
+            this.setState({
+                arr:['我出来了']
+            })
+        }.bind(this),1000) // 让this指向原处 
     }
 
     componentDidMount() {
@@ -18,9 +24,9 @@ class Index extends Component {
 
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
+    // shouldComponentUpdate(nextProps, nextState) {
 
-    }
+    // }
 
     componentWillUpdate(nextProps, nextState) {
 
@@ -33,11 +39,25 @@ class Index extends Component {
     componentWillUnmount() {
 
     }
-
     render() {
         return (
             <div>
                 增加用户
+                {
+                    this.state.arr.length > 0 
+                    ?
+                    <div>
+                        <ul>
+                            {
+                                this.state.arr.map(item=>{
+                                    return <li>{item}</li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                    :
+                    ""
+                }
             </div>
         )
     }
